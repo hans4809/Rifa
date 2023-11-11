88bd834dae9e4a80cbbf09363ca1e2f2cc623109
catsnakedog <103244627+catsnakedog@users.noreply.github.com>
2023-11-11 18:41:21 +0900

    feat : 세이브 데이터 값 정리

--- Source/Rifa/MyGameInstance.h
+++ Source/Rifa/MyGameInstance.h
@@ -4,22 +4,30 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
-#include "Engine/DataTable.h"
 #include "MyGameInstance.generated.h"
 /**
  * 
  */
 
-USTRUCT()
-struct FSavePointData : public FTableRowBase
+UENUM()
+enum class Item : uint8 // æ«±‚¿Ã∏ß_∏Óπ¯_≥Ù≥∑¿Ã
 {
-	GENERATED_BODY()
-
-		UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		FVector Position;
-
-		UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		TArray<int> ItemList;
+	Piano_1_Medium,
+	WindInstrument_1_Medium,
+	Bell_1_High,
+	Voice_1_Women,
+	Piano_1_High,
+	StringInstrument_1_Medium,
+	Piano_2_Low,
+	WindInstrument_2_Medium,
+	Voice_2_Men,
+	PercussionInstrument_1_Low,
+	StringInstrument_2_Medium,
+	Piano_2_High,
+	StringInstrument_3_High,
+	Bell_2_High,
+	PercussionInstrument_3_High,
+	StringInstrument_4_High,
 };
 
 UCLASS()
@@ -30,5 +38,11 @@ class RIFA_API UMyGameInstance : public UGameInstance
 public:
 	UMyGameInstance();
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector Position;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString SoundTrack;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TMap <Item, bool> ItemMap;
 private:
 };
