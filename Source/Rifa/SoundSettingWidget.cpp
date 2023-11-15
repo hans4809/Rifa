523645a62cfd1fd6d74fa61d6dc3d64780b1629e
hans4809 <hans4809@gmail.com>
2023-11-16 00:00:32 +0900

    feat : SoundSetting UI 작업 중...

---
+++ Source/Rifa/SoundSettingWidget.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SoundSettingWidget.h"
+#include "Components/Button.h"
+
+void USoundSettingWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	/*ResetButton = Cast<UButton>(GetWidgetFromName(TEXT("ResetButton")));
+	LoadButton = Cast<UButton>(GetWidgetFromName(TEXT("LoadButton")));
+	SettingButton = Cast<UButton>(GetWidgetFromName(TEXT("SettingButton")));
+	QuitButton = Cast<UButton>(GetWidgetFromName(TEXT("QuitButton")));*/
+}
