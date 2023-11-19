f7793e67804c0b8ff6b6abcea96a171c2a31d6fb
hans4809 <hans4809@gmail.com>
2023-11-19 21:59:07 +0900

    feat : soundmanager(?) 해보는 중

---
+++ Source/Rifa/BGMAudioComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/AudioComponent.h"
+#include "BGMAudioComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RIFA_API UBGMAudioComponent : public UAudioComponent
+{
+	GENERATED_BODY()
+	
+};
--- Source/Rifa/MainGameMode.cpp
+++ Source/Rifa/MainGameMode.cpp
@@ -4,6 +4,9 @@
 #include "MainGameMode.h"
 #include "RifaMainHUD.h"
 #include <Kismet/GameplayStatics.h>
+#include "BGMSoundActor.h"
+#include "SoundManagerInstance.h"
+#include "Components/AudioComponent.h"
 
 void AMainGameMode::BeginPlay()
 {
@@ -15,4 +18,15 @@ void AMainGameMode::BeginPlay()
 	MainHUDWidgetAsset->Init();
 	Cast<APlayerController>(UGameplayStatics::GetPlayerController(GetWorld(), 0))->SetInputMode(FInputModeUIOnly());
 	Cast<APlayerController>(UGameplayStatics::GetPlayerController(GetWorld(), 0))->bShowMouseCursor = true;
+	GetGameInstance()->GetSubsystem<USoundManagerInstance>()->BGMSound = Cast<UAudioComponent>(UGameplayStatics::GetActorOfClass(GetWorld(), ABGMSoundActor::StaticClass()));
+	GetGameInstance()->GetSubsystem<USoundManagerInstance>()->SFXSound = Cast<UAudioComponent>(UGameplayStatics::GetActorOfClass(GetWorld(), ABGMSoundActor::StaticClass()));
+	if (GetGameInstance()->GetSubsystem<USoundManagerInstance>()->BGMSound != nullptr)
+	{
+		GetGameInstance()->GetSubsystem<USoundManagerInstance>()->BGMSound->Play();
+		UE_LOG(LogTemp, Log, TEXT("BGM Played"));
+	}
+	else 
+	{
+		UE_LOG(LogTemp, Log, TEXT("BGM is null"));
+	}
 }
