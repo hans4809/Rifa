102d28259302d883ee9bf4cec1313ebc4624c2e5
hans4809 <58892107+hans4809@users.noreply.github.com>
2024-07-18 13:18:50 +0900

    HUD PlayerController에 옮기는 작업 중

--- Source/Rifa/Character/RifaPlayerController.cpp
+++ Source/Rifa/Character/RifaPlayerController.cpp
@@ -2,4 +2,24 @@
 
 
 #include "Character/RifaPlayerController.h"
+#include <Widget/GameHUD.h>
 
+ARifaPlayerController::ARifaPlayerController()
+{
+	static ConstructorHelpers::FClassFinder<UUserWidget> GameHUDWidget(TEXT("/Script/UMGEditor.WidgetBlueprint'/Game/BluePrint/UI/WG_GameHUD.WG_GameHUD_C'"));
+	if (GameHUDWidget.Succeeded())
+		GameHUDWidgetClass = GameHUDWidget.Class;
+}
+
+void ARifaPlayerController::BeginPlay()
+{
+	if(IsValid(GameHUDWidgetClass))
+		GameHUDWidgetAsset = Cast<UGameHUD>(CreateWidget(GetWorld(), GameHUDWidgetClass));
+
+	if(IsValid(GameHUDWidgetAsset))
+		GameHUDWidgetAsset->Init();
+}
+
+void ARifaPlayerController::Tick(float DeltaTime)
+{
+}
--- Source/Rifa/Character/RifaPlayerController.h
+++ Source/Rifa/Character/RifaPlayerController.h
@@ -13,5 +13,18 @@ UCLASS()
 class RIFA_API ARifaPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+public:
+	ARifaPlayerController();
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widget", meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<UUserWidget> GameHUDWidgetClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widget", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UGameHUD> GameHUDWidgetAsset;
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 };
